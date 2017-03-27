2c49bcd311c8cf6066f7cd734180be11946fb56d
Paulo I. Carrasco González <paulo.carrasco.gonzalez@gmail.com>
2017-03-27 10:11:54 -0300

    Commit

---
+++ src/main/java/HolaMundoJenkins/HMJ/App.java
@@ -0,0 +1,13 @@
+package HolaMundoJenkins.HMJ;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
